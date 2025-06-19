69a2b3f96b0b06a9e98483bf686dbea4e5809996
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-06-19 10:56:25 +0000

    testipv6

--- test/internet/test-net-blocklist.js
+++ test/internet/test-net-blocklist.js
@@ -6,13 +6,10 @@ console.log(cache);
 const assert = require('assert');
 const { BlockList } = require('net');
 const data = [
-  // IPv4 examples
   'Subnet: IPv4 192.168.1.0/24',
   'Address: IPv4 10.0.0.5',
   'Range: IPv4 192.168.2.1-192.168.2.10',
   'Range: IPv4 10.0.0.1-10.0.0.10',
-
-  // IPv6 examples
   'Subnet: IPv6 2001:0db8:85a3:0000:0000:8a2e:0370:7334/64',
   'Address: IPv6 2001:0db8:85a3:0000:0000:8a2e:0370:7334',
   'Range: IPv6 2001:0db8:85a3:0000:0000:8a2e:0370:7334-2001:0db8:85a3:0000:0000:8a2e:0370:7335',
@@ -29,3 +26,4 @@ test2.fromJSON(JSON.parse(test.toJSON()));
 test2.fromJSON(test.toJSON());
 test3.fromJSON(data);
 assert.strictEqual(test2.rules, test.rules);
+assert.strictEqual(test3.rules,data)
